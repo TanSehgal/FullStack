8700d1cf2c28a262a5eec53eef8e88ce588ed32b
TanSehgal <121184811+TanSehgal@users.noreply.github.com>
2025-10-28 22:02:12 +0530

    Create index.js

---
+++ React/practice3/index.js
@@ -0,0 +1,8 @@
+// Module entry point for Person Class Hierarchy
+// This file can be used to import and run the demonstration
+
+export { Person, Student, Teacher } from './person.js';
+export { default as runDemo } from './demo.js';
+
+// To run the demo, execute this file with Node.js:
+// node demo.js
